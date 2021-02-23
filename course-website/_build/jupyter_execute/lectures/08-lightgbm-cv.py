86e8287caa4f574db4cbfd4a835d6ccfb658c66e
SDSTony <sung8906@gmail.com>
2021-02-23 17:22:21 +0900

    build html and reorganize toc

---
+++ course-website/_build/jupyter_execute/lectures/08-lightgbm-cv.py
@@ -0,0 +1,131 @@
+# 데모
+
+## 라이브러리 import 및 설정
+
+%reload_ext autoreload
+%autoreload 2
+%matplotlib inline
+
+import lightgbm as lgb
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+import numpy as np
+from pathlib import Path
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+import seaborn as sns
+import warnings
+
+rcParams['figure.figsize'] = (16, 8)
+plt.style.use('fivethirtyeight')
+pd.set_option('max_columns', 100)
+pd.set_option("display.precision", 4)
+warnings.simplefilter('ignore')
+
+## 학습데이터 로드
+
+[03-pandas-eda.ipynb](https://github.com/kaggler-tv/dku-kaggle-class/blob/master/notebook/03-pandas-eda.ipynb)에서 생성한 `feature.csv` 피처파일 사용
+
+data_dir = Path('../data/dacon-dku')
+feature_dir = Path('../build/feature')
+val_dir = Path('../build/val')
+tst_dir = Path('../build/tst')
+sub_dir = Path('../build/sub')
+
+trn_file = data_dir / 'train.csv'
+tst_file = data_dir / 'test.csv'
+sample_file = data_dir / 'sample_submission.csv'
+
+target_col = 'class'
+n_fold = 5
+n_class = 3
+seed = 42
+
+algo_name = 'lgbcv'
+feature_name = 'feature'
+model_name = f'{algo_name}_{feature_name}'
+
+feature_file = feature_dir / f'{feature_name}.csv'
+p_val_file = val_dir / f'{model_name}.val.csv'
+p_tst_file = tst_dir / f'{model_name}.tst.csv'
+sub_file = sub_dir / f'{model_name}.csv'
+
+df = pd.read_csv(feature_file, index_col=0)
+print(df.shape)
+df.head()
+
+y = df[target_col].values[:320000]
+df.drop(target_col, axis=1, inplace=True)
+trn = df.iloc[:320000].values
+tst = df.iloc[320000:].values
+feature_name = df.columns.tolist()
+print(y.shape, trn.shape, tst.shape)
+
+## Stratified K-Fold Cross Validation
+
+cv = StratifiedKFold(n_splits=n_fold, shuffle=True, random_state=seed)
+
+## LightGBM 모델 학습
+
+p_val = np.zeros((trn.shape[0], n_class))
+p_tst = np.zeros((tst.shape[0], n_class))
+for i, (i_trn, i_val) in enumerate(cv.split(trn, y), 1):
+    print(f'training model for CV #{i}')
+    clf = lgb.LGBMClassifier(objective='multiclass',
+                             n_estimators=1000,
+                             num_leaves=64,
+                             learning_rate=0.1,
+                             min_child_samples=10,
+                             subsample=.5,
+                             subsample_freq=1,
+                             colsample_bytree=.8,
+                             random_state=seed,
+                             n_jobs=-1)
+    clf.fit(trn[i_trn], y[i_trn],
+            eval_set=[(trn[i_val], y[i_val])],
+            eval_metric='multiclass',
+            early_stopping_rounds=10)
+    
+    p_val[i_val, :] = clf.predict_proba(trn[i_val])
+    p_tst += clf.predict_proba(tst) / n_fold
+
+print(f'{accuracy_score(y, np.argmax(p_val, axis=1)) * 100:.4f}%')
+
+print(p_val.shape, p_tst.shape)
+
+np.savetxt(p_val_file, p_val, fmt='%.6f', delimiter=',')
+np.savetxt(p_tst_file, p_tst, fmt='%.6f', delimiter=',')
+
+## 피처 중요도 시각화
+
+imp = pd.DataFrame({'feature': df.columns, 'importance': clf.feature_importances_})
+imp = imp.sort_values('importance').set_index('feature')
+imp.plot(kind='barh')
+
+## 제출 파일 생성
+
+sub = pd.read_csv(sample_file, index_col=0)
+print(sub.shape)
+sub.head()
+
+sub[target_col] = np.argmax(p_tst, axis=1)
+sub.head()
+
+sub[target_col].value_counts()
+
+sub.to_csv(sub_file)
+
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+8장. Cross-Validation, Stacking (2) <08-lr-cv>
+8장. Cross-Validation, Stacking (3) <08-rf-cv>
+8장. Cross-Validation, Stacking (4) <08-stacking-lgb>
+8장. Cross-Validation, Stacking (5) <08-stacking-lr>
+```
